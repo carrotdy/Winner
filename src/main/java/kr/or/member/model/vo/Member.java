a3963a1cd192f73684e82374bfe7ea7a9bea05ec
DESKTOP-C55UU5R\홍다영 <dyhong9212@gmail.com>
2022-01-12 00:18:04 +0900

    22-01-12 기본설정, index.jsp 메인화면

---
+++ src/main/java/kr/or/member/model/vo/Member.java
@@ -0,0 +1,19 @@
+package kr.or.member.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private int MemberNo;		//회원번호
+	private String memberId;    //아이디
+	private String memberPw;    //비밀번호
+	private String memberName;  //이름
+	private String phone;		//핸드폰번호
+	private String postCode;    //우편번호
+	private String address1;    //상세주소1
+	private String address2;    //상세주소2
+	private String email;       //이메일
+	private String enrollDate;  //가입일
+	private String filePath;    //파일경로
+	private int memberLevel;    //회원레밸
+}
